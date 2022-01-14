63d0fd7f04aba242e5ac3bf7c0d9984c63c38b42
Gino Carrasco Mori <gino.carrasco@tranzact.net>
2022-01-14 14:10:38 -0500

    added counter

---
+++ components/Counter.js
@@ -0,0 +1,51 @@
+import { LitElement, html, css } from "lit";
+
+export class Counter extends LitElement {
+  static properties = {
+    count: { type: Number },
+  };
+
+  static styles = css`
+    * {
+      font-size: 200%;
+    }
+
+    span {
+      width: 4rem;
+      display: inline-block;
+      text-align: center;
+    }
+
+    button {
+      width: 64px;
+      height: 64px;
+      border: none;
+      border-radius: 10px;
+      background-color: seagreen;
+      color: white;
+    }
+  `;
+
+  constructor() {
+    super();
+    this.count = 0;
+  }
+
+  inc() {
+    this.count++;
+  }
+
+  dec() {
+    this.count--;
+  }
+
+  render() {
+    return html`
+      <button @click="${this.dec}">-</button>
+      <span>${this.count}</span>
+      <button @click="${this.inc}">+</button>
+    `;
+  }
+}
+
+customElements.define("lit-counter", Counter);
