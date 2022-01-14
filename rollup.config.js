4debaa1539c3e8d6de277687c4f0c4b7b698b425
Gino Carrasco Mori <gino.carrasco@tranzact.net>
2022-01-14 15:11:49 -0500

    updated build

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,17 @@
 import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 
 export default {
 	// If using any exports from a symlinked project, uncomment the following:
 	// preserveSymlinks: true,
 	  input: ['components/index.js'],
 	  output: {
-		  file: 'build.js',
-		  format: 'es',
+		  file: 'bundle.js',
+		  format: 'iife',
 		  sourcemap: true
 	  },
 	  plugins: [
-		resolve()
+		resolve(),
+		commonjs()
 	  ]
   };
